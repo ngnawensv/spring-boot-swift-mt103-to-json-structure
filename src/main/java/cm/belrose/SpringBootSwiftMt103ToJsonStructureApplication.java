c66c0bbca6bdfbbc579955155b8dfc6c678aec1a
Ngnawen Samuel <ngnawens@gmail.com>
2024-10-31 23:05:18 -0400

    First commit

---
+++ src/main/java/cm/belrose/SpringBootSwiftMt103ToJsonStructureApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSwiftMt103ToJsonStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSwiftMt103ToJsonStructureApplication.class, args);
+	}
+
+}
